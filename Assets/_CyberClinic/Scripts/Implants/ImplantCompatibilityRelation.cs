dd2614f5d3f3997c07f70cd82be9983948cd64cb
huseyinaydin25-alt <huseyinaydin25@gmail.com>
2026-05-27 01:39:04 +0300

    implants: add compatibility relation types

---
+++ Assets/_CyberClinic/Scripts/Implants/ImplantCompatibilityRelation.cs
@@ -0,0 +1,11 @@
+namespace CyberClinic.Implants
+{
+    public enum ImplantCompatibilityRelation
+    {
+        PerfectMatch = 0,
+        Acceptable = 1,
+        SlotStress = 2,
+        BodyProblemConflict = 3,
+        HiddenSlotConflict = 4
+    }
+}
