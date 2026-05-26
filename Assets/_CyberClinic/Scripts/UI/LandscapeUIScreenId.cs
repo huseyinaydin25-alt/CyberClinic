dad2e2c2578a2b2ce39b4f7985a221ab50ad8817
huseyinaydin25-alt <huseyinaydin25@gmail.com>
2026-05-27 02:30:52 +0300

    ui: add landscape ui screen ids

---
+++ Assets/_CyberClinic/Scripts/UI/LandscapeUIScreenId.cs
@@ -0,0 +1,10 @@
+namespace CyberClinic.UI
+{
+    public enum LandscapeUIScreenId
+    {
+        PatientFile = 0,
+        ImplantSelection = 1,
+        RiskPreview = 2,
+        ResultPanel = 3
+    }
+}
