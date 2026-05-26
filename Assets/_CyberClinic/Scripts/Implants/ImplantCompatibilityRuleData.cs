0dd3967638454872995c7f0aa0dca4e11c91010e
huseyinaydin25-alt <huseyinaydin25@gmail.com>
2026-05-27 01:39:56 +0300

    implants: expand compatibility rule data

--- Assets/_CyberClinic/Scripts/Implants/ImplantCompatibilityRuleData.cs
+++ Assets/_CyberClinic/Scripts/Implants/ImplantCompatibilityRuleData.cs
@@ -1,4 +1,5 @@
 using CyberClinic.Core;
+using CyberClinic.Localization;
 using CyberClinic.Patients;
 using UnityEngine;
 
@@ -8,13 +9,25 @@ namespace CyberClinic.Implants
     public class ImplantCompatibilityRuleData : ScriptableObject, IIdentifiable
     {
         [SerializeField] string _id;
-        [SerializeField] BodySlot _slot;
-        [SerializeField] string _conflictingBodyProblemId;
-        [SerializeField] float _compatibilityPenalty = 0.25f;
+        [SerializeField] LocalizedTextRef _label;
+        [SerializeField] LocalizedTextRef _description;
+        [SerializeField] BodySlot _patientSlot;
+        [SerializeField] BodySlot _implantSlot;
+        [SerializeField] ImplantCompatibilityRelation _relation;
+        [SerializeField] float _compatibilityModifier;
+        [SerializeField] float _bodyStressModifier;
+        [SerializeField] string _bodyProblemId;
+        [SerializeField] string _hiddenConditionId;
 
         public string Id => _id;
-        public BodySlot Slot => _slot;
-        public string ConflictingBodyProblemId => _conflictingBodyProblemId;
-        public float CompatibilityPenalty => _compatibilityPenalty;
+        public LocalizedTextRef Label => _label;
+        public LocalizedTextRef Description => _description;
+        public BodySlot PatientSlot => _patientSlot;
+        public BodySlot ImplantSlot => _implantSlot;
+        public ImplantCompatibilityRelation Relation => _relation;
+        public float CompatibilityModifier => _compatibilityModifier;
+        public float BodyStressModifier => _bodyStressModifier;
+        public string BodyProblemId => _bodyProblemId;
+        public string HiddenConditionId => _hiddenConditionId;
     }
 }
