ae75610b5877757e77fa59751a513a84756cf009
huseyinaydin25-alt <huseyinaydin25@gmail.com>
2026-05-27 03:32:52 +0300

    audio: add clinic audio categories

---
+++ Assets/_CyberClinic/Scripts/Audio/ClinicAudioCategory.cs
@@ -0,0 +1,14 @@
+namespace CyberClinic.Audio
+{
+    public enum ClinicAudioCategory
+    {
+        Ui = 0,
+        Scan = 1,
+        Warning = 2,
+        Surgery = 3,
+        Patient = 4,
+        Result = 5,
+        Ambience = 6,
+        Music = 7
+    }
+}
