ee4e66fe8e8301d3ee66c9cf09a09e06a6012d5f
huseyinaydin25-alt <huseyinaydin25@gmail.com>
2026-05-27 03:32:16 +0300

    audio: add clinic audio event ids

---
+++ Assets/_CyberClinic/Scripts/Audio/ClinicAudioEventId.cs
@@ -0,0 +1,16 @@
+namespace CyberClinic.Audio
+{
+    public enum ClinicAudioEventId
+    {
+        UiClick = 0,
+        PanelOpen = 1,
+        ScanStart = 2,
+        ScanComplete = 3,
+        WarningPulse = 4,
+        OperationCommit = 5,
+        OperationSuccess = 6,
+        OperationFailure = 7,
+        AmbienceLoop = 8,
+        MusicState = 9
+    }
+}
