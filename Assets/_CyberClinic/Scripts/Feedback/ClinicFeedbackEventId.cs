b81440388f91c7f2e320b9e405984569636cc037
huseyinaydin25-alt <huseyinaydin25@gmail.com>
2026-05-27 03:05:54 +0300

    feedback: add clinic feedback event ids

---
+++ Assets/_CyberClinic/Scripts/Feedback/ClinicFeedbackEventId.cs
@@ -0,0 +1,14 @@
+namespace CyberClinic.Feedback
+{
+    public enum ClinicFeedbackEventId
+    {
+        OpenPatientFile = 0,
+        BasicScan = 1,
+        DeepScan = 2,
+        SelectImplant = 3,
+        RiskPreviewUpdated = 4,
+        CommitOperation = 5,
+        OperationResult = 6,
+        PanelTransition = 7
+    }
+}
