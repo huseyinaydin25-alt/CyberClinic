c4438df18e7402b21177fdb33c31eddaee193192
huseyinaydin25-alt <huseyinaydin25@gmail.com>
2026-05-27 05:06:03 +0300

    online: add rank entry dto

---
+++ Assets/_CyberClinic/Scripts/Online/OnlineRankEntry.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace CyberClinic.Online
+{
+    [Serializable]
+    public class OnlineRankEntry
+    {
+        public string PlayerId;
+        public string NameKey;
+        public int Value;
+        public int Position;
+    }
+}
