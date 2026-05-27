65fe29fa372aa0af4f8c22199ea986aac72d8da1
huseyinaydin25-alt <huseyinaydin25@gmail.com>
2026-05-27 03:06:20 +0300

    feedback: add clinic feedback categories

---
+++ Assets/_CyberClinic/Scripts/Feedback/ClinicFeedbackCategory.cs
@@ -0,0 +1,13 @@
+namespace CyberClinic.Feedback
+{
+    public enum ClinicFeedbackCategory
+    {
+        Scan = 0,
+        Glitch = 1,
+        WarningPulse = 2,
+        ResultReveal = 3,
+        PanelTransition = 4,
+        ImplantInstall = 5,
+        PatientReaction = 6
+    }
+}
